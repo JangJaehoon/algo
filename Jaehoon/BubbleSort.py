778422d1044dfae49e5f11f047f8826f722b0e2d
jjhgwx <jjhgwx@gmail.com>
2020-12-24 23:20:41 +0900

    Bubble Sort

--- Jaehoon/BubbleSort.py
+++ Jaehoon/BubbleSort.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
 # UTF-8 encoding when using korean
-# -*- coding: utf-8 -*-
-# UTF-8 encoding when using korean
 a = int(input())
-print(a)
 def inputArray():
 	for _ in range(a):
 		grid = list(map(int, input().split()))
-		print(grid)
+		return grid
 
 def bubbleSort(grid):
 	for i in range(len(grid)-1):
 		for j in range(len(grid)-i-1):
 			if grid[j] > grid[j+1]:
 				grid[j], grid[j+1] = grid[j+1], grid[j]
-				print(grid)
+	grid = list(map(str, grid))
+	print(" ".join(grid))
+
 
 if __name__ == "__main__":
-	inputArray()
-	bubbleSort()
+	bubbleSort(inputArray())
